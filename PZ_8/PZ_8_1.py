1f740c6e977f4a37076ea821b935d1891f1d975f
user <davidxa112233@mail.ru>
2021-12-15 01:21:22 +0300

    Изменённое решение на правильное по условию

--- PZ_8/PZ_8_1.py
+++ PZ_8/PZ_8_1.py
@@ -1,7 +1,8 @@
 # Выполнить сортировку словаря d = {'a': 1, 'b': 2, 'c': 3}
 d = {'a': 1, 'b': 2, 'c': 3}
+d1 = {}
 k = sorted(d, reverse=True)
 for i in range(len(d)):
-    d.update({k[i]: 1+i})
-print(d)
+    d1.update({k[i]: 3-i})
+print(d1)
 # Я не совсем понял условие. Надеюсь, что решил правильно
