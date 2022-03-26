068f5373fa9e9ac36f640eb5147d90d9d377e73a
L0g1sT <92188765+L0g1sT@users.noreply.github.com>
2022-03-26 13:43:25 +0300

    Delete PZ_14_1.py

--- PZ_14/PZ_14_1.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-p = re.compile(r"[8(863)]+[0-9]+[0-9]+[0-9]+[-]+[0-9]+[0-9]+[-]+[0-9]+[0-9]")
-with open('hotline_1.txt', 'r', encoding='utf-8') as file:
-    text = file.read()
-
-print(p.findall(text))
-print(len(p.findall(text)))
